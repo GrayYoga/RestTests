ce847100baa6fb618c43fe3f6030c0ecd9a1f7df
s.boiko <s.boiko@rogii.com>
2024-12-24 23:48:00 +0400

    Implement and use infinite todos-feeder

--- src/test/kotlin/performance/PostPerfScenario.kt
+++ src/test/kotlin/performance/PostPerfScenario.kt
@@ -1,14 +1,46 @@
 package performance
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import dto.Todo
 import io.gatling.javaapi.core.CoreDsl
 import io.gatling.javaapi.http.HttpDsl
-import utils.TestData
+import managers.TodoManager
+import utils.checkSuccess
+import utils.listOfEntity
 
-class PostPerfScenario {
-    fun scn() = CoreDsl.scenario("performance.PostPerfSimulation")
-        .exec(
-            HttpDsl.http("post todos")
-            .post("/todos")
-            .body(CoreDsl.StringBody(TestData.todo().toString()))
-        )
+
+object PostPerfScenario {
+    private val mapper = jacksonObjectMapper()
+    private var initialId = TodoManager().getTodos(limit = 10000)
+        .checkSuccess()
+        .listOfEntity(Todo::class.java)
+        .maxOfOrNull { it.id!! }
+        ?: 1u
+
+    private fun feeder(): Iterator<Map<String, String>> = iterator {
+        while (true) {
+            yield(
+                mapOf(
+                    "todo" to mapper.writeValueAsString(
+                        Todo(
+                            id = initialId++,
+                            text = "Todo $initialId",
+                            completed = (initialId % 2u).toInt() == 0
+                        )
+                    )
+                )
+            )
+        }
+    }
+
+    fun scn() =
+        CoreDsl.scenario("performance.PostPerfSimulation")
+            .feed(feeder())
+            .exec(
+                HttpDsl.http("post todos")
+                    .post("/todos")
+                    .body(
+                        CoreDsl.StringBody("#{todo}")
+                    )
+            )
 }
