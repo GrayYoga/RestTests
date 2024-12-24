78f04a9bdc81c9feaa2bc747710cd83f3fe0873f
s.boiko <s.boiko@rogii.com>
2024-12-24 23:48:47 +0400

    WIP: working simulation

--- src/test/kotlin/performance/PostPerfSimulation.kt
+++ src/test/kotlin/performance/PostPerfSimulation.kt
@@ -1,13 +1,25 @@
 package performance
 
-import io.gatling.javaapi.core.CoreDsl.atOnceUsers
+import io.gatling.javaapi.core.CoreDsl
+import io.gatling.javaapi.core.OpenInjectionStep
 import io.gatling.javaapi.core.Simulation
+
 
 class PostPerfSimulation : Simulation() {
 
     init {
         setUp(
-            listOf(PostPerfScenario().scn().injectOpen(atOnceUsers(1)).protocols(BaseProtocol().httpProtocol()))
+            listOf(
+                PostPerfScenario
+                    .scn()
+                    .injectOpen(
+//                        OpenInjectionStep.atOnceUsers(1)
+                        CoreDsl.rampUsersPerSec(0.0)
+                            .to(1000.0)
+                            .during(1 * 60L),
+                    )
+                    .protocols(BaseProtocol().httpProtocol())
+            )
         )
     }
 }
